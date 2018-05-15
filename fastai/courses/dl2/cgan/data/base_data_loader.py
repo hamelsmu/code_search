36f45fad90042a1439dc0d7641483eb7a15d8ea1
Hamel Husain <hamel.husain@gmail.com>
2018-05-14 21:12:26 -0700

    updated notebooks

--- fastai/courses/dl2/cgan/data/base_data_loader.py
+++
@@ -1,5 +0,0 @@
-class BaseDataLoader():
-    def __init__(self): pass
-    def load_data(): return None
-    def initialize(self, opt): self.opt = opt
-
---
+++ notebooks/general_utils.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import logging
+
+def read_training_files(data_path):
+    """
+    Read data from directory
+    """
+    PATH = Path(data_path)
+
+    with open(PATH/'train.function', 'r') as f:
+        t_enc = f.readlines()
+
+    with open(PATH/'valid.function', 'r') as f:
+        v_enc = f.readlines()
+
+    # combine train and validation and let keras split it randomly for you
+    tv_enc = t_enc + v_enc
+
+    with open(PATH/'test.function', 'r') as f:
+        h_enc = f.readlines()
+
+    with open(PATH/'train.docstring', 'r') as f:
+        t_dec = f.readlines()
+
+    with open(PATH/'valid.docstring', 'r') as f:
+        v_dec = f.readlines()
+
+    # combine train and validation and let keras split it randomly for you
+    tv_dec = t_dec + v_dec
+
+    with open(PATH/'test.docstring', 'r') as f:
+        h_dec = f.readlines()
+
+    logging.warning(f'Num rows for encoder training + validation input: {len(tv_enc):,}')
+    logging.warning(f'Num rows for encoder holdout input: {len(h_enc):,}')
+
+    logging.warning(f'Num rows for decoder training + validation input: {len(tv_dec):,}')
+    logging.warning(f'Num rows for decoder holdout input: {len(h_dec):,}')
+
+    return tv_enc, h_enc, tv_dec, h_dec
